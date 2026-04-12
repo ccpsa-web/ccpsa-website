4c1900c2b81f56990a56165cc3e804313df731be
ccpsa-web <dboe@critcaremd.com>
2026-04-12 11:50:09 -0600

    fix: remove unused content prop from our-team page

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -1,9 +1,8 @@
-import { getProviders, getPageContent } from '@/lib/content';
+import { getProviders } from '@/lib/content';
 import TeamClient from './TeamClient';
 
 export default function OurTeam() {
   const providers = getProviders();
-  const content = getPageContent('our-team');
 
-  return <TeamClient providers={providers} content={content} />;
+  return <TeamClient providers={providers} />;
 }
